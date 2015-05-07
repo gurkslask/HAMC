0b3b123ff561a0c8b024bec037db00ee2903cef1
Alexander Svensson <gurkslask@gmail.com>
2015-05-07 20:07:01 +0200

    Changed filepath

--- cold_retain.py
+++ cold_retain.py
@@ -6,11 +6,11 @@ def cold_retain(object, list_with_vars):
     data_dict = {}
     for var in list_with_vars:
        data_dict[var] = object.__dict__[var]
-    with open('coldretaintest', 'w') as json_file:
+    with open('ColdRetain.json', 'w') as json_file:
         json_file.write(json.dumps(data_dict))
 
 def cold_retain_load(object):
-    with open('coldretaintest', 'r') as json_file:
+    with open('ColdRetain.json', 'r') as json_file:
         json_data = json.loads(json_file.read())
     return json_data
 
