3555f3ff7ab924c1f0da985e1d404afd15705553
Alexander Svensson <gurkslask@gmail.com>
2016-03-17 14:01:41 +0100

    End of the day save

--- connect_to_socket.py
+++ connect_to_socket.py
@@ -10,7 +10,7 @@ import sys
 def call_server(message):
     message = pickle.dumps(message)
     HOST = '127.0.0.1'    # The remote host
-    PORT = 5004              # The same port as used by the server
+    PORT = 5008              # The same port as used by the server
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((HOST, PORT))
     s.sendall(message)
