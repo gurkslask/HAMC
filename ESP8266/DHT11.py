8ad0e82b789014d624637ed16d5d1544a8e12cb1
Alexander Svensson <gurkslask@gmail.com>
2016-11-17 22:09:38 +0100

    End of day save

--- ESP8266/DHT11.py
+++ ESP8266/DHT11.py
@@ -1,22 +1,27 @@
 """Reads temperature and humidity from a DHT11 with a ESP8266 via MQTT."""
-from machine import Pin
+import machine
 from dht import DHT11
 import time
 import ubinascii
 from umqtt.simple import MQTTClient
 
-d = DHT11(Pin(0))
-SERVER = '192.168.1.139'
+# d = DHT11(machine.Pin(0))
+SERVER = '192.168.1.8'
 CLIENT_ID = ubinascii.hexlify(machine.unique_id())
-TOPIC = b"kgrund"
+TOPIC = b'kgrund/fukt'
+
 
 def main(server=SERVER):
-    """Measure and talk to MQTT broker"""
+    """Measure and talk to MQTT broker."""
     c = MQTTClient(CLIENT_ID, server)
-    c.connect
-    print("Connected to {}".format(server))
-    while true:
+    c.connect()
+    # print('Connected to {}'.format(server))
+    """while True:
         d.measure()
-        print("humidity: {}".format(d.humidity())
-        c.publish(TOPIC, bytes(d.humidity()))
-        time.sleep(20)
+        c.publish(TOPIC, 42)
+        time.sleep(20)"""
+    c.publish(TOPIC, b"hello")
+    c.disconnect()
+
+if __name__ == '__main__':
+    main(SERVER)
