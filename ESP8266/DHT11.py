d744adf2e8366aea57aef0e4f9ebf95f99f51f6a
Alexander Svensson <gurkslask@gmail.com>
2016-11-18 08:25:55 +0100

    Update DHT11.py

--- ESP8266/DHT11.py
+++ ESP8266/DHT11.py
@@ -8,17 +8,20 @@ from umqtt.simple import MQTTClient
 # d = DHT11(machine.Pin(0))
 SERVER = '192.168.1.8'
 CLIENT_ID = ubinascii.hexlify(machine.unique_id())
-TOPIC = b'kgrund/fukt'
+TOPIC_HUMIDITY = b'kgrund/fukt'
+TOPIC_TEMPERATURE = b'kgrund/temp'
 
 
 def main(server=SERVER):
     """Measure and talk to MQTT broker."""
     c = MQTTClient(CLIENT_ID, server)
     c.connect()
-    # print('Connected to {}'.format(server))
+    print('Connected to {}'.format(server))
     """while True:
+        """Measure and then publish values to the broker every 20 seconds."""
         d.measure()
-        c.publish(TOPIC, d.humidity())
+        c.publish(TOPIC_HUMIDITY, d.humidity())
+        c.publish(TOPIC_TEMPERATURE, d.temperature())
         time.sleep(20)"""
     c.publish(TOPIC, b"hello")
     c.disconnect()
